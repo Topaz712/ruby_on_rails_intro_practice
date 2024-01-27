10a9042b5a68be7713e4b9255796b8815bf83d4d
Topaz712 <90583331+Topaz712@users.noreply.github.com>
2024-01-27 01:19:31 -0600

    updated user.rb for callbacks section

--- user.rb
+++ user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  validate :name_cannot_contain_numbers
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
-  def name_cannot_contain_numbers
-    if name.match(/\d/)
-      errors.add(:name, "cannot contain numbers")
-    end
+  before_create :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
   end
 end
